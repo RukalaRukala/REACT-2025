044411cafe2c267bcd388ea0b81f3c57d50e7fa5
Mikalai Rusakovich <m.rusakovich@andersenlab.com>
2025-07-20 23:52:42 +0300

    feat: set up jest config

---
+++ jest.config.ts
@@ -0,0 +1,40 @@
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'jest-environment-jsdom',
+  transform: {
+    '^.+\\.(ts|tsx)$': [
+      'ts-jest',
+      {
+        tsconfig: './tsconfig.test.json',
+      },
+    ],
+  },
+  moduleNameMapper: {
+    '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/src/test/__mocks__/fileMock.js',
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    '\\.module\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+  },
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx,ts,tsx}',
+    '!src/**/*.test.{js,jsx,ts,tsx}',
+    '!src/**/*.spec.{js,jsx,ts,tsx}',
+    '!src/main.{js,jsx,ts,tsx}',
+    '!src/setupTests.{js,ts}',
+    '!src/**/*.d.ts',
+    '!src/vite-env.d.ts',
+    '!src/test/**/*',
+  ],
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov', 'html'],
+  coverageThreshold: {
+    global: {
+      statements: 80,
+      branches: 50,
+      functions: 50,
+      lines: 50,
+    },
+  },
+};
