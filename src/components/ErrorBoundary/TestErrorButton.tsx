47b0959bd890a63c6491fd58d161d3a01847c5e7
Mikalai Rusakovich <m.rusakovich@andersenlab.com>
2025-07-28 22:17:37 +0300

    feat: change error button to a functional component

--- src/components/ErrorBoundary/TestErrorButton.tsx
+++ src/components/ErrorBoundary/TestErrorButton.tsx
@@ -1,40 +1,23 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import { TEST_ERROR_MESSAGES } from '../Search/Search.const.tsx';
 import styles from './TestErrorButton.module.scss';
 
-interface TestErrorButtonState {
-  shouldThrowError: boolean;
-}
+const TestErrorButton = () => {
+  const [shouldThrowError, setShouldThrowError] = useState<boolean>(false);
 
-class TestErrorButton extends Component<
-  Record<string, never>,
-  TestErrorButtonState
-> {
-  constructor(props: Record<string, never>) {
-    super(props);
-    this.state = {
-      shouldThrowError: false,
-    };
-  }
-
-  handleThrowError = (): void => {
-    this.setState({ shouldThrowError: true });
+  const handleThrowError = (): void => {
+    setShouldThrowError(true);
   };
 
-  render() {
-    if (this.state.shouldThrowError) {
-      throw new Error(TEST_ERROR_MESSAGES.ERROR_MESSAGE);
-    }
-
-    return (
-      <button
-        onClick={this.handleThrowError}
-        className={styles.testErrorButton}
-      >
-        {TEST_ERROR_MESSAGES.BUTTON_TEXT}
-      </button>
-    );
+  if (shouldThrowError) {
+    throw new Error(TEST_ERROR_MESSAGES.ERROR_MESSAGE);
   }
-}
+
+  return (
+    <button onClick={handleThrowError} className={styles.testErrorButton}>
+      {TEST_ERROR_MESSAGES.BUTTON_TEXT}
+    </button>
+  );
+};
 
 export default TestErrorButton;
