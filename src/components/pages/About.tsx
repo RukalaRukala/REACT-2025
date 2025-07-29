16fd1fb13586fec40ad03f37930b09251824ab5b
Mikalai Rusakovich <m.rusakovich@andersenlab.com>
2025-07-29 23:49:09 +0300

    feat: add about styles

--- src/components/pages/About.tsx
+++ src/components/pages/About.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
+import styles from './About.module.scss';
 
 const About: React.FC = () => (
-  <div style={{ padding: '2rem' }}>
-    <h1>About</h1>
-    <p>Author: Rusakovich Mikalai</p>
+  <div className={styles.about}>
+    <h1 className={styles.about__title}>About</h1>
+    <p className={styles.about__author}>Author: Rusakovich Mikalai</p>
     <p>
-      Курс:{' '}
+      Course:{' '}
       <a
-        href="https://rs.school/react/"
+        href="https://rs.school/courses/reactjs"
         target="_blank"
         rel="noopener noreferrer"
+        className={styles.about__link}
       >
         RS School React
       </a>
