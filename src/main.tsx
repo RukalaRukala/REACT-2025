63899c03aa07d7b37e419288a7815748c44cfa61
Mikalai Rusakovich <m.rusakovich@andersenlab.com>
2025-08-25 16:43:13 +0300

    feat: start commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,7 @@
 import { createRoot } from 'react-dom/client';
-import './index.scss';
 import App from './App.tsx';
-import ErrorBoundary from './components/ErrorBoundary/ErrorBoundary.tsx';
 
 const rootElement = document.getElementById('root');
 if (rootElement) {
-  createRoot(rootElement).render(
-    <ErrorBoundary>
-      <App />
-    </ErrorBoundary>
-  );
+  createRoot(rootElement).render(<App />);
 }
